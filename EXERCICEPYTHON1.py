b85895638b488582960cc2a54f3cf3c07cd7f075
Naelle69 <113608387+Naelle69@users.noreply.github.com>
2022-09-30 17:23:22 +0200

    Create EXERCICEPYTHON1.py

---
+++ EXERCICEPYTHON1.py
@@ -0,0 +1,34 @@
+from ast import main
+import numbers
+from re import S
+from tkinter.tix import INTEGER
+
+#Exo 1:
+# création d'une variable qui demande à l’utilisateur 3 nombres entiers positifs et plus petits que 40000
+number1=1000
+number2=500
+number3=450
+#faire la somme des variables 
+somme=(number1+number2+number3)
+s=somme/3
+#faire la moyenne de somme
+print(s)
+#effectue la moyenne de ces trois nombres
+
+
+#exo 2:
+#écrire un nombre entier
+
+number=int (input("saisir un nombre entier">=0))
+if number>=0
+    print("Ce nombre est positif ou nul")
+else: 
+    print("Ce nombre est négatif")
+
+
+
+
+
+
+
+
